00e716b9786cb493d6d9344922ea513df8ab6bbf
ashiato45 <katiiijp@gmail.com>
2023-02-17 03:03:27 +0900

    trackpc

--- trackpc.py
+++ trackpc.py
@@ -1,9 +1,12 @@
 import gdb
 
-# gdb.execute("break main")
-gdb.execute("break loader.c:153")
-# gdb.execute("run")
-gdb.execute("run ./simple")
+# gdb -x trackpc.py ./simple
+gdb.execute("break main")
+gdb.execute("run")
+
+#  gdb -x trackpc.py ./loader > test1.txt
+# gdb.execute("break loader.c:153")
+# gdb.execute("run ./simple")
 
 for i in range(1000):
     gdb.execute("stepi")
