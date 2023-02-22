ba840d6b562d8e52227587e877e0d0542db4fb32
ashiato45 <katiiijp@gmail.com>
2023-02-23 00:06:39 +0900

    hoge

--- trackpc.py
+++ trackpc.py
@@ -1,12 +1,13 @@
 import gdb
 
 # gdb -x trackpc.py ./simple
-# gdb.execute("break main")
+gdb.execute("break main")
 gdb.execute("break *0x00400a50")
 gdb.execute("run")
 
 #  gdb -x trackpc.py ./loader > test1.txt
 # gdb.execute("break loader.c:153")
+# gdb.execute("break *0x400a80")
 # gdb.execute("run ./simple")
 
 for i in range(10000000):
@@ -14,5 +15,6 @@ for i in range(10000000):
     gdb.execute("x/i $pc")
     gdb.execute('printf "pc=0x%x, rip=0x%x, rdi=0x%x, rax=0x%x, rbx=0x%x, rsp=0x%x, rsi=0x%x\n", $pc, $rip, $rdi, $rax, $rbx, $rsp, $rsi')
     # gdb.execute('x/x $sp')
+    gdb.execute("x/4x 0x7fffffffde30")
 
 gdb.execute("quit")
