81ffdbf36caf09854aa9f2b5e47dd95d7ff43be9
ashiato45 <katiiijp@gmail.com>
2023-02-23 16:07:09 +0900

    argv

--- trackpc.py
+++ trackpc.py
@@ -1,20 +1,22 @@
 import gdb
 
 # gdb -x trackpc.py ./simple
-gdb.execute("break main")
-gdb.execute("break *0x00400a50")
-gdb.execute("run")
+# gdb.execute("break main")
+# gdb.execute("break *0x00400a50")
+# gdb.execute("run")
 
 #  gdb -x trackpc.py ./loader > test1.txt
+# gdb.execute("break loader.c:main")
 # gdb.execute("break loader.c:153")
+gdb.execute("break *0x8009a0")
 # gdb.execute("break *0x400a80")
-# gdb.execute("run ./simple")
+gdb.execute("run ./simple hoge")
 
 for i in range(10000000):
     gdb.execute("stepi")
     gdb.execute("x/i $pc")
     gdb.execute('printf "pc=0x%x, rip=0x%x, rdi=0x%x, rax=0x%x, rbx=0x%x, rsp=0x%x, rsi=0x%x\n", $pc, $rip, $rdi, $rax, $rbx, $rsp, $rsi')
-    # gdb.execute('x/x $sp')
-    gdb.execute("x/4x 0x7fffffffde30")
+    gdb.execute('x/8x $sp')
+    # gdb.execute("x/4x 0x7fffffffde30")
 
 gdb.execute("quit")
